b5cf5ec73e71bcea4d0f666508485a94a58a4932
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-04-04 17:17:21 +0200

    Comments and documentation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# isolations-hjalpen
-Hack the Crisis Sweden prototype for matchmaking
+# grannleveransen.se / isolations-hjälpen
+Hack the Crisis Sweden prototype for matchmaking - grannleveransen.se / isolations-hjälpen
 
 ## To run locally
 Install node on your machine.
@@ -8,4 +8,18 @@ Run ```npm install``` in repositiory folder
 Run ```npm run start``` to start a local webserver. It prints the URL to visit in your browser.
 
 ## To deploy
-You need access, ask Erik Larsson to deploy to heroku hello from tim timmen hater
+<<<<<<< HEAD
+You need access, ask Erik Larsson to deploy to heroku
+
+
+### Sanity check in terminal
+Run these commands in git bash terminal to make sure everything is installed on your local machine
+$ node --version
+v12.13.1
+
+$ npm --version
+6.12.1
+
+$ git --version
+git version 2.24.0.windows.2
+>>>>>>> Comments and documentation
--- index.js
+++ index.js
@@ -4,21 +4,28 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
+/*  Volatile list of helpers in the system - this should be replaced with a database,
+	since inactivity or any error in the code restarts the webserver and wipes the list */
 let helpers = []
 
+/* 	Serve the static folder /static/
+	/static/index.html -> oursite.com/index.html */
 app.use(express.static('static'))
 
-
+/*	Use node module body-parser to be able to read request.body in the request handlers */
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+/* API */
 app.post('/helper', helper)
 app.get('/helper-list', helperList)
 app.get('/clear', clear)
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+/* Start the server */
+app.listen(port, () => console.log(`App grannhjalpen.se listening at http://localhost:${port}`))
 
 
+/* API to create a new helper */
 function helper(request, response)  {  
   const { position } = request.body
   
@@ -30,10 +37,13 @@ function helper(request, response)  {
 }
 
 
+/* API list all helpers */
 function helperList(request, response)  {
   response.status(200).json(helpers)
 }
 
+
+/* Clear all helpers */
 function clear(request, response)  {
   helpers = []
   response.status(200).send()
