2565e2d61463267658cdff08f7ae93c25787425a
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-04-04 13:35:41 +0200

    API

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # isolations-hjalpen
 Hack the Crisis Sweden prototype for matchmaking
+
+## To run locally
+Install node on your machine.
+Run ```npm install``` in repositiory folder
+
+Run ```npm run start``` to start a local webserver. It prints the URL to visit in your browser.
+
+## To deploy
+You need access, ask Erik Larsson to deploy to heroku
--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static('.', 'static'))
+let helpers = []
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/helper', helper)
+app.get('/helper-list', helperList)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
+function helper(request, response)  {  
+  const { position } = request.body
+  
+  helpers.push({
+  	position: position
+  })
+
+  response.status(200).send()
+}
+
+
+async function helperList(request, response)  {
+  response.status(200).json(helpers)
+} 
+
+//app.use(express.static('.', 'static'))
