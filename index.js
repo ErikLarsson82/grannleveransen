fd561496ebd70e0c1d7d73e85a74d00ef6c24a2a
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-04-04 12:17:26 +0200

    Static folder and ignore file

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World Isolations-Hjälpen Sverige!'))
+app.use(express.static('.', 'static'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
