0a40c2d4cd5be5bef0c6fb69046419e605b16475
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-04-06 12:51:04 +0200

    Proper Routing

--- express.js
+++ express.js
@@ -6,6 +6,17 @@ const port = process.env.PORT || 3000
 /* 	Serve the static folder /static/
 	/static/index.html -> oursite.com/index.html */
 app.use(express.static('build'))
+app.use("/needer/welcome", express.static('build'))
+app.use("/needer/createuser", express.static('build'))
+app.use("/needer/searchmatch", express.static('build'))
+app.use("/needer/foundnomatch", express.static('build'))
+app.use("/needer/foundlistofmatches", express.static('build'))
+app.use("/needer/goodsreceived", express.static('build'))
+app.use("/helper/createuser", express.static('build'))
+app.use("/helper/dashboardhelper", express.static('build'))
+app.use("/helper/searchmatch", express.static('build'))
+app.use("/helper/establishcontacthelper", express.static('build'))
+app.use("/helper/contactfound", express.static('build'))
 
 /* Start the server */
 app.listen(port, () => console.log(`Frontend for grannleveransen.se port ${port}`))
