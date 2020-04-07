1ff768acf94f31b2477a185588a5dd4b57f8b132
TimTimmen <timtimmen@protonmail.ch>
2020-04-07 22:32:21 +0200

    hopefully working now!

--- src/DashboardHelper.js
+++ src/DashboardHelper.js
@@ -7,7 +7,7 @@ import colors from './colors';
 
 const RADIUS = 600
 const isLocalhost = window.location.host.indexOf("localhost") !== -1
-const API = isLocalhost ? 'http://localhost:3000' : 'http://grenas-be.herokuapp.com'
+const API = isLocalhost ? 'http://localhost:789' : 'https://grannleveransen-be.herokuapp.com'
 
 // const API = "http://localhost:3000"
 console.log(API)
