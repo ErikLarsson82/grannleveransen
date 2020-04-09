1556a004d1664dfd567552b9145d0a20603423f8
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-04-09 14:35:31 +0200

    Build scripts for development and production

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/App.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+module.exports = (env, argv) => {
+  return {
+    entry: './src/App.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.css$/i,
+          use: ['css-loader']
         }
-      },
-      {
-        test: /\.css$/i,
-        use: ['css-loader']
-      }
-    ]
-  }
-};
+      ]
+    },
+    watch: argv.mode === 'development'
+  };
+}
