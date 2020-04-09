6ffe9b74273a4d860e856b0740bb5253379b0b62
Erik Larsson <erik.mattias.larsson@gmail.com>
2020-04-09 14:02:42 +0200

    Remove so much crap like you wouldnt beleive it. No more create-react-app and manual webpack build

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
 # grannleveransen.se
-Hack the Crisis Sweden prototype for matchmaking - grannleveransen.se
-
-This version is called 'version-1' and is probably obsolete since we use react now
+Hack the Crisis Sweden prototype for matchmaking
 
 ## To run locally
-* Install node on your machine.
-* Run ```npm install``` to install dependencies locally
-* Run ```npm start``` in repositiory folder
-
-Run ```npm run dev``` to start a local webserver for development. It will open a tab for you in your browser and use hot-reload.
 
 ## To deploy
 You need access, ask Erik Larsson or Tim Fredriksson to deploy to heroku
@@ -23,13 +16,3 @@ $ npm --version
 
 $ git --version
 git version 2.24.0.windows.2
-
-## Merged create-react-app
-Find documentation about that here https://github.com/facebook/create-react-app
-
-## Old name
-isolations-hjälpen
-
-## Backend repository
-https://github.com/ErikLarsson82/grannleveransen-be
-Deployed at https://grannleveransen-be.herokuapp.com/
--- express.js
+++ express.js
@@ -1,11 +1,58 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const http = require('http')
+const WebSocket = require('ws')
 
-const app = express()
 const port = process.env.PORT || 3000
+const app = express()
+const httpServer = http.createServer(app)
+const wss = new WebSocket.Server({
+    'server': httpServer
+})
+//
+/*  Volatile list of connections in the system - this should be replaced with a database,
+	since inactivity or any error in the code restarts the webserver and wipes the list */
+let connections = []
 
-/* 	Serve the static folder /static/
-	/static/index.html -> oursite.com/index.html */
-app.use(express.static('build'))
+wss.on('connection', ws => {
+  let id
+
+  ws.on('message', str => {
+    const messageObj = JSON.parse(str)
+    if (messageObj.event === 'handshake') {
+      id = messageObj.id
+      connections.push({
+        id: messageObj.id,
+        hook: ws,
+        position: messageObj.position,
+        message: messageObj.message
+      })
+    }
+  })
+  ws.on('close', () => {
+    connections = connections.filter(x => x.id !== id)
+  })
+})
+
+function websocketMessageClient(msgObj) {
+  connections.forEach(connection => {
+    if (connection.id === msgObj.id) {
+      connection.hook.send(JSON.stringify(msgObj.message))
+    }
+  })
+}
+
+/*
+app.use(function(req, res, next) {
+  // Change this URL to the one where the frontend code is deployed
+  // For example https://isolations-hjalpen.herokuapp.com for the deployed code
+  // and http://localhost:3000 for local development
+  res.header("Access-Control-Allow-Origin", "https://isolations-hjalpen.herokuapp.com")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+*/
+app.use("/", express.static('build'))
 app.use("/needer/welcome", express.static('build'))
 app.use("/needer/createuser", express.static('build'))
 app.use("/needer/searchmatch", express.static('build'))
@@ -18,5 +65,38 @@ app.use("/helper/searchmatch", express.static('build'))
 app.use("/helper/establishcontacthelper", express.static('build'))
 app.use("/helper/contactfound", express.static('build'))
 
-/* Start the server */
-app.listen(port, () => console.log(`Frontend for grannleveransen.se port ${port}`))
+/*	Use node module body-parser to be able to read request.body in the request handlers */
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+/* API */
+app.post('/message', message)
+app.get('/needers', neederList)
+app.get('/clear', clear)
+
+httpServer.listen(port)
+console.log(`Server listening on port ${port}`)
+
+/* API to create a new helper */
+function message(request, response)  {
+  const { id, message } = request.body
+  console.log('message incoming', id, message)
+  response.status(200).send()
+  websocketMessageClient({ id, message })
+}
+
+/* API list all connections */
+function neederList(request, response)  {
+  const removeHook = connections.map( x => ({
+    id: x.id,
+    position: x.position,
+    message: x.message
+  }) )
+  response.status(200).json(removeHook)
+}
+
+/* Clear all connections */
+function clear(request, response)  {
+  connections = []
+  response.status(200).send()
+} 
